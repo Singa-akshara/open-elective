8ea2c107dccb52bbf1291fb3e227b73ccc3b0f58
Singa-akshara <155072100+Singa-akshara@users.noreply.github.com>
2024-06-19 00:34:05 +0530

    Update LogoutPage.js

--- src/LogoutPage.js
+++ src/LogoutPage.js
@@ -1,18 +1,46 @@
+// import React from 'react';
+// import './LogoutPage.css';
+
+// function LogoutPage() {
+//   // Define the logout message
+//   const logoutMessage = "You have been logged out.";
+  
+//   // Define any additional messages or elements
+//   const additionalMessage = "Thank you for visiting. We hope to see you again soon!";
+  
+//   return (
+//     <div className="logout-page">
+//       <div className="logout-message">{logoutMessage}</div>
+//       <div className="additional-message">{additionalMessage}</div>
+//       <button className="logout-button">Login Again</button>
+//     </div>
+//   );
+// }
+
+// export default LogoutPage;
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './LogoutPage.css';
 
 function LogoutPage() {
+  const navigate = useNavigate();
+
+  // Handle navigation to login page
+  const loginAgainHandler = () => {
+    navigate('/login');
+  };
+
   // Define the logout message
   const logoutMessage = "You have been logged out.";
-  
+
   // Define any additional messages or elements
   const additionalMessage = "Thank you for visiting. We hope to see you again soon!";
-  
+
   return (
     <div className="logout-page">
       <div className="logout-message">{logoutMessage}</div>
       <div className="additional-message">{additionalMessage}</div>
-      <button className="logout-button">Login Again</button>
+      <button className="logout-button" onClick={loginAgainHandler}>Login Again</button>
     </div>
   );
 }
